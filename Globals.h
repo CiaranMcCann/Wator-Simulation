1903e9f628b741356e774c2dcd7b527d7e1460ff
Keith Cully <kcul.dev@gmail.com>
2012-10-11 14:33:43 +0100

    Fixed fish drawing issue, changed grid and window size.

--- Globals.h
+++ Globals.h
@@ -6,17 +6,14 @@
  *  A file which defines the variables shared throughout the Wator simulation.
  */
 
-float const WindowHeight = 600.0f;
-float const WindowWidth = 600.0f;
+float const WindowHeight = 900.0f;
+float const WindowWidth = 900.0f;
 
 // NOTE: const in C means read-only, the value can still actually change
-#define GRID_ROWS  100
-#define GRID_COLUMNS 100
+#define GRID_ROWS  1000
+#define GRID_COLUMNS 1000
 
 #define N_SHARKS 100
 #define N_FISH 40
-
-int const NumberOfSharks = 10;
-int const NumberOfFish = 10;
 
 #endif
