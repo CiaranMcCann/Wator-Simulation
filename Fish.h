96248c00052ec810902a411fd0c97599da24bb38
jami3 <jamiemulligan4@gmail.com>
2012-10-19 09:46:30 +0100

    Further comments

--- Fish.h
+++ Fish.h
@@ -41,7 +41,6 @@ Fish * fishFactory(int x, int y)
  *  @param int y The x position of the tile
  *  @param Fish *fish Pointer to the current fish
  */
-
 void moveFish(int x, int y, Fish *fish)
 {
     fish->mSpawnCounter +=1;
@@ -59,11 +58,8 @@ void moveFish(int x, int y, Fish *fish)
 	moveFishPointerTo(newPosition, fish);
     	fish->pos.X = x;
     	fish->pos.Y = y;
-        // Updated fish age
     }
 }
-
-
 
 /*! \ Checks the surrounding grid positions for a free tile
  *  Store the possible directions in a character array and increase the 'available' directions
@@ -74,7 +70,6 @@ void moveFish(int x, int y, Fish *fish)
  *  @param int y The y position of the tile
  *  @param Fish *fish Pointer to the current fish
  */
-
 void updateFish(int x, int y, Fish *pFish)
 {
     // Make sure updated is set to 0 
@@ -82,13 +77,13 @@ void updateFish(int x, int y, Fish *pFish)
         return;
 
     char direction[4];
-    int available = 0;
+    int available = 0; //!< Number of available directions
 
     // Add all available directions to a char array
     // Increase the available parameter
     if(checkTileForEntity(x, y+1) == 0){
         direction[available] = 'N';
-        available++;
+        available++; // Increase the number of available directions
     } 
     
     if(checkTileForEntity(x, y-1) == 0){
@@ -107,7 +102,7 @@ void updateFish(int x, int y, Fish *pFish)
         available++;
     }
 
-    if(i>0) // I
+    if(i>0) 
     {
         available = rand() % available;
         x = pFish->pos.X;
@@ -116,16 +111,16 @@ void updateFish(int x, int y, Fish *pFish)
         // Set the direction
         switch( direction[available] )
         {
-                case 'N': // North
+                case 'N': // Direction is North
                     y+=1;
                     break;
-                case 'S': // South
+                case 'S': // Direction is South
                     y-=1;
                     break;
-                case 'E': // East
+                case 'E': // Direction is East
                     x+=1;
                     break;
-                case 'W': // West
+                case 'W': // Direction is West
                     x-=1;
                     break;
                 default :
