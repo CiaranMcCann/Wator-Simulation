047d8264ed1a821b3276eac3e85b947ae170508f
Keith Cully <kcul.dev@gmail.com>
2012-10-19 16:56:56 +0100

    Added frame counters and frame per second logging.

--- main.c
+++ main.c
@@ -4,25 +4,61 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "Globals.h"
 #include "GridPosition.h"
 #include "Drawing.h"
 #include "World.h"
 
+/*!
+ *  \brief Saves a performance log for the simulation.
+ *
+ *  \param fpsLogs The array of logged frames per second.
+*/
+int saveLogTofile(int * fpsLogs)
+{
+	//Rows = .......\n
+	//Columns = ........\n
+	//Threads = ........\n
+	//20 lines of fps
+	FILE * pFile = fopen( "Performance_data.log", "w" );
+	fprintf( pFile, "Rows: %d\n", GRID_ROWS ); 
+	fprintf( pFile, "Columns: %d\n", GRID_COLUMNS ); 
+	fprintf( pFile, "Threads: SERIAL\n" ); 
+	int count = 0;
+	for (count = 0; count < SIMULATION_LENGTH; ++count)
+	{
+		fprintf( pFile, "%d\n", fpsLogs[count] ); 
+	}
+	close( pFile );
+}
+
 /*! \brief The main entry point for the program.
  */
 int main(int argc, char *argv[])
 {
-    populateWorld(N_FISH,N_SHARKS);
-
+	printf( "Simulation will run for %d seconds.\n", SIMULATION_LENGTH );
+	
+	int run = 1;
+	int seconds = 0;
+	double secondTimer = 0.0;
+	int frameCounter = 0;
+	time_t currentTime = 0;
+	time_t previousTime = 0;	
+	int framesPerSecond[SIMULATION_LENGTH];
+		
+	populateWorld(N_FISH,N_SHARKS);
+		
 	if (InitializeOpenGL())
 	{
 		int count = 0;
-		while (1)
+		while (run)
 		{
+			++count;
 			if (count == CYCLES_PER_FRAME)
-			{
+			{				
+				count = 0;
 				updateWorld();
 				if (DRAW_GRID)
 				{
@@ -34,11 +70,31 @@ int main(int argc, char *argv[])
 				}
 				else
 				{
-					printf("Running\n");
+					frameCounter++;
+					currentTime = time(NULL);					
+					if (previousTime)
+					{
+						secondTimer += difftime(currentTime, previousTime);	
+					}
+					previousTime = time(NULL);				
+					
+					if (secondTimer >= 1.0)
+					{
+						secondTimer -= 1.0;
+						framesPerSecond[seconds] = frameCounter;
+						++seconds;
+						// printf( "%d\n", frameCounter);						
+						frameCounter = 0;						
+						if (seconds == SIMULATION_LENGTH)
+						{
+							run = 0;
+							saveLogTofile( framesPerSecond );
+							printf( "Performance data saved to log file.\n");
+						}
+					}
 				}
-				count = 0;
 			}
-			++count;
 		}
 	}
+	printf( "Simulation complete.\n");
 }
