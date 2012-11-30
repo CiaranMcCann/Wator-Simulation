83cf7c87561a052bdf243c6baaa63fdd7e343c4f
jami3 <jamiemulligan4@gmail.com>
2012-11-30 12:04:46 +0000

    Automated benchmarking

---
+++ run_benchmark.sh
@@ -0,0 +1,40 @@
+python write_global.py 16 1
+sh run.sh
+python write_global.py 16 2
+sh run.sh
+python write_global.py 16 4
+sh run.sh
+python write_global.py 16 8
+sh run.sh
+python write_global.py 128 1
+sh run.sh
+python write_global.py 128 2
+sh run.sh
+python write_global.py 128 4
+sh run.sh
+python write_global.py 128 8
+sh run.sh
+python write_global.py 512 1
+sh run.sh
+python write_global.py 512 2
+sh run.sh
+python write_global.py 512 4
+sh run.sh
+python write_global.py 512 8
+sh run.sh
+python write_global.py 2048 1
+sh run.sh
+python write_global.py 2048 2
+sh run.sh
+python write_global.py 2048 4
+sh run.sh
+python write_global.py 2048 8
+sh run.sh
+python write_global.py 4096 1
+sh run.sh
+python write_global.py 4096 2
+sh run.sh
+python write_global.py 4096 4
+sh run.sh
+python write_global.py 4096 8
+sh run.sh
---
+++ write_global.py
@@ -0,0 +1,42 @@
+import sys
+
+GRID_SIZE = sys.argv[1]
+THREADS = sys.argv[2]
+NSHARKS = GRID_SIZE
+NFISH = str(int(GRID_SIZE)*10)
+
+
+f = open('/home/lab422/dev/Wator-Simulation/Globals.h','r')
+fwrite = open('/home/lab422/dev/Wator-Simulation/Globals.h','w')
+
+fwrite.write(str('#ifndef GLOBALS_H')+'\n')
+fwrite.write(str('#define GLOBALS_H')+'\n'+'\n')
+
+fwrite.write(str('float const WindowHeight = 800.0f;')+'\n')
+fwrite.write(str('float const WindowWidth = 800.0f;')+'\n'+'\n')	
+
+fwrite.write(str('int numThreads = ')+ THREADS+';\n'+'\n');	
+
+fwrite.write(str('#define NUMBER_OF_RUNS 5')+'\n')		
+
+fwrite.write(str('#define GRID_ROWS ')+ GRID_SIZE +'\n')		
+fwrite.write(str('#define GRID_COLUMNS ')+ GRID_SIZE+'\n')
+fwrite.write(str('#define CYCLES_PER_FRAME 1')+'\n'+'\n')
+
+fwrite.write(str('#define N_SHARKS ')+NSHARKS +'\n')
+fwrite.write(str('#define N_FISH ')+ NFISH +'\n')
+fwrite.write(str('#define FISH_SPAWNRATE 3')+'\n')
+fwrite.write(str('#define SHARK_SPAWNRATE 7')+'\n')
+fwrite.write(str('#define SHARK_STARVERATE 6')+'\n'+'\n')
+fwrite.write(str('#define SIMULATION_LENGTH 5')+'\n'+'\n')	
+
+fwrite.write(str('#define DRAW_GRID 0')+'\n'+'\n')
+
+
+fwrite.write(str('#endif'))
+
+	
+f.close()
+fwrite.close()
+
+
